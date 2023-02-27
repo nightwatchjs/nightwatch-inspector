820ad25ca02d3c5eae09b97281a07e0adeb837d3
Prudhvi <prudhvi@browserstack.com>
2023-02-27 18:21:48 +0530

    Added eslint

--- src/background.js
+++ src/background.js
@@ -4,15 +4,15 @@ chrome.runtime.onConnect.addListener(function (port) {
   var extensionListener = function (message, sender, sendResponse) {
     // The original connection event doesn't include the tab ID of the
     // DevTools page, so we need to send it explicitly.
-    if (message.action == 'INIT') {
+    if (message.action === 'INIT') {
       connections[message.tabId] = port;
+      
       return;
-    }
-    else {
-      // Send all messages recieved from devtools to content js
-      chrome.tabs.sendMessage(message.tabId, message, sendResponse);
-    }
-  }
+    } 
+    // Send all messages recieved from devtools to content js
+    chrome.tabs.sendMessage(message.tabId, message, sendResponse);
+    
+  };
   
   // Listen to messages sent from the DevTools page
   port.onMessage.addListener(extensionListener);
@@ -22,7 +22,7 @@ chrome.runtime.onConnect.addListener(function (port) {
     var tabs = Object.keys(connections);
     for (var i=0, len=tabs.length; i < len; i++) {
       
-      if (connections[tabs[i]] == port) {
+      if (connections[tabs[i]] === port) {
         chrome.tabs.sendMessage(Number(tabs[i]), {
           tabId: tabs[i],
           action: 'EXPLORE_MODE',
--- src/selectorPlayground.js
+++ src/selectorPlayground.js
@@ -3,4 +3,4 @@
 // chrome.extension.*
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("Selector Playground", null, "panel.html", function(panel) {});
+chrome.devtools.panels.create('Selector Playground', null, 'panel.html', function() {});
