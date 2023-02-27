2623d0d26e39b13867f789aa4faff71be869e9b9
Prudhvi <prudhvi@browserstack.com>
2023-02-27 10:03:59 +0530

    Add: input suggestions

--- src/panel.js
+++ src/panel.js
@@ -2,48 +2,14 @@ const tabID = chrome.devtools.inspectedWindow.tabId;
 const exploreModeId = 'exploreMode';
 const tryNightwatchCommandId = 'tryNightwatchCommand';
 const nightwatchCommandId = 'nightwatchCommand';
+const input = document.getElementById("nightwatchCommand");
+const suggestion = document.getElementById("suggestion");
 const commandHistory = new Set();
 let EXPLORE_MODE = false;
 const portNumber = 8080;
 let webSocket;
+let words = []
 
-function connectWebSocket(port) {
-  return new Promise((resolve, reject) => {
-    const socket = new WebSocket(`ws://localhost:${port}`);
-    socket.onopen = () => {
-      resolve(socket);
-      console.log(`Hurray, Connected to Nightwatch Server localhost:${port}`);
-      webSocket = socket;
-    };
-  
-    socket.onclose = () => {
-      console.log(`Disconnected from Nightwatch Server localhost:${port}`);
-    };
-
-    socket.onmessage = ((msg) => {
-      const {result = null, error = null, executedCommand} = JSON.parse(msg.data);
-      console.log(msg.data);
-      document.getElementById('commandResult').textContent = result;
-      if (!error && !commandHistory.has(executedCommand)) {
-        addRowInCommand(executedCommand);
-        commandHistory.add(executedCommand);
-      }
-    
-      sendMessageToBackground('EXPLORE_MODE', true);
-    });
-
-    socket.onerror = (err) => {
-      console.error(`WebSocket error: ${err}`);
-      socket.close();
-
-      if (port < portNumber + 100) {
-        connectWebSocket(port + 1)
-          .then(resolve)
-          .catch(reject);
-      }
-    };
-  });
-}
 
 connectWebSocket(portNumber)
   .then((socket) => {
@@ -77,6 +43,52 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
 
 document.getElementById(exploreModeId).addEventListener('click', clickOnExploreMode);
 document.getElementById(tryNightwatchCommandId).addEventListener('click', tryNightwatchCommand);
+
+function connectWebSocket(port) {
+  return new Promise((resolve, reject) => {
+    const socket = new WebSocket(`ws://localhost:${port}`);
+    socket.onopen = () => {
+      resolve(socket);
+      console.log(`Hurray, Connected to Nightwatch Server localhost:${port}`);
+      socket.send('commandlist');
+      webSocket = socket;
+    };
+  
+    socket.onclose = () => {
+      console.log(`Disconnected from Nightwatch Server localhost:${port}`);
+    };
+
+    socket.onmessage = ((msg) => {
+      const {result = null, error = null, executedCommand, commandList = []} = JSON.parse(msg.data);
+      const commandResultElement = document.getElementById('commandResult');
+      if (commandList.length) {
+        words = [...commandList];
+        
+        return;
+      }
+      console.log(result);
+      commandResultElement.textContent = result;
+      if (!error && !commandHistory.has(executedCommand)) {
+        addRowInCommand(executedCommand);
+        commandHistory.add(executedCommand);
+        commandResultElement.style.color = '#4e8865';
+      } else if (error) {
+        commandResultElement.style.color = '#B80D0D';
+      }
+    
+      sendMessageToBackground('EXPLORE_MODE', true);
+    });
+
+    socket.onerror = (err) => {
+      console.error(`WebSocket error: ${err}`);
+      socket.close();
+
+      connectWebSocket(port + 1)
+        .then(resolve)
+        .catch(reject);
+    };
+  });
+}
 
 
 function tryNightwatchCommand() {
@@ -168,15 +180,15 @@ function addRow(selector) {
   copyButton.addEventListener('click', clickOnCopy);
   
   var newCell = newRow.insertCell();
-  newCell.style.width = '50%';
+  newCell.style.width = '70%';
   newCell.appendChild(document.createTextNode(selector));
   
-  newCell = newRow.insertCell();
-  newCell.style.width = '25%';
-  newCell.appendChild(document.createTextNode(timestamp));
+  // newCell = newRow.insertCell();
+  // newCell.style.width = '25%';
+  // newCell.appendChild(document.createTextNode(timestamp));
   
   newCell = newRow.insertCell();
-  newCell.style.width = '25%';
+  newCell.style.width = '30%';
   newCell.appendChild(highlightButton);
   newCell.appendChild(copyButton);
 }
@@ -186,3 +198,39 @@ function addRowInCommand(command) {
   const selectorTable = document.getElementById('commandTable');
   selectorTable.value = command + '\r\n' + selectorTable.value;
 }
+
+window.onload = () => {
+  input.value = 'browser.title()';
+  clearSuggestion();
+};
+
+const clearSuggestion = () => {
+  suggestion.innerHTML = "";
+};
+
+//Execute function on input
+input.addEventListener('input', (e) => {
+  clearSuggestion();
+  const inputValue = input.value.split('.').at(-1);
+  
+  for (let i in words) {
+    //check if input matches with any word in words array
+    if (words[i].startsWith(inputValue) && input.value != '' && inputValue != '') {
+      //display suggestion
+      const suggestionValue = words[i].replace(inputValue, '');
+      suggestion.innerHTML = input.value + suggestionValue;
+      break;
+    }
+  }
+});
+
+//Complete predictive text on enter key
+input.addEventListener('keydown', (e) => {
+  //When user presses enter and suggestion exists
+  if (e.keyCode == 39 && suggestion.innerText != '') {
+    e.preventDefault();
+    input.value = suggestion.innerText;
+    //clear the suggestion
+    clearSuggestion();
+  }
+});
