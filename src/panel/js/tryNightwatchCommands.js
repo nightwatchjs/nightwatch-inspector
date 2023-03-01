cfba0cb3a2441ab243311715c1000e67a5f36be3
Harshit Agrawal <harshit.a@browserstack.com>
2023-03-02 01:33:32 +0530

    tests for extension UI using Nightwatchjs

--- src/panel/js/tryNightwatchCommands.js
+++ src/panel/js/tryNightwatchCommands.js
@@ -10,10 +10,10 @@ const nightwatchCommandId = 'nightwatchCommand';
 function tryNightwatchCommand() {
   const nightwatchCommandElement = document.getElementById(nightwatchCommandId);
   const nightwatchCommand = nightwatchCommandElement.value;
-
+  webSocket.send(nightwatchCommand);
+  
   // setting explore mode false when trying out nightwatch commands
   sendMessageToBackground('EXPLORE_MODE', false);
-  webSocket.send(nightwatchCommand);
 }
 
 document.getElementById(tryNightwatchCommandId).addEventListener('click', tryNightwatchCommand);
